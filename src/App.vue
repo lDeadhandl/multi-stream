1387df59c2f4f1bb8d03b1ca770b62326e48f621
lDeadhandl <stefan.vladetic7@gmail.com>
2021-10-10 20:14:17 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <MultiStream msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import '../lib/twitch.v1.js'
+import MultiStream from './components/MultiStream.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MultiStream
+  }
+}
+
+</script>
+
+<style>
+#app {
+  height: inherit;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MultiStream.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container">
+      <div class="main-row">
+        <div id="twitch-embed" class="tile">
+        </div>
+        <div id="twitch-embed-t" class="tile">
+        </div>
+      </div>
+
+      <div class="sub-row">
+        <div id="twitch-embed-3" class="tile">
+        </div>
+        <div class="tile">
+        </div>
+        <div class="tile">
+        </div>
+        <div class="tile">
+        </div>
+        <div class="tile">
+        </div>
+                <div class="tile">
+        </div>
+
+      </div>
+  </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'MultiStream',
+        props: {
+            msg: String
+        },
+        mounted() {
+            let options = {
+                width: '100%',
+                height: '100%',
+                channel: "Nickmercs",
+                layout: "video"
+            };
+            new window.Twitch.Embed("twitch-embed", options);
+            // var player = new window.Twitch.Player("<player div ID>", options);
+            // console.log(player)
+
+            let options2 = {
+                width: '100%',
+                height: '100%',
+                channel: "xqcow",
+                layout: "video"
+            };
+            new window.Twitch.Embed("twitch-embed-t", options2);
+            // let player2 = new window.Twitch.Player("twitch-embed-t", options2);
+            // console.log(player2)
+                        let options3 = {
+                width: '100%',
+                height: '100%',
+                channel: "imaqtpie",
+                layout: "video"
+            };
+            new window.Twitch.Embed("twitch-embed-3", options3);
+        }
+    }
+    
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
+.main-row {
+    display: flex;
+    flex-grow: 1;
+    flex-direction: row;
+    height: 500px;
+}
+
+.sub-row {
+    display: flex;
+    flex-grow: 1;
+    height: 200px;
+    margin-top: 10px;
+}
+
+.tile {
+    flex-grow: 1;
+    background-color: aqua;
+    margin: 0px 5px;
+}
+
+.aqua-div {
+    flex-grow: 2;
+    background-color: aqua;
+}
+
+.brown-div {
+    flex-grow: 1;
+    background-color:brown;
+}
+</style>
